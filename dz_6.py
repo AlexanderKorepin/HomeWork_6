575540902f8009a90b46a72404cf0969fc3499dd
Alexander <1987kerykery@mail.ru>
2023-05-15 21:37:15 +1100

    HomeWork_6

--- dz_6.py
+++ dz_6.py
@@ -27,6 +27,24 @@ def zadacha_3():
     print(f'Колличество совпадений с введенным числом: {count} раз(-а)')
   else:
    print(f'В массиве  НЕТ совпадений из последовательности чисел {nunber}') 
-zadacha_3()
 #Задача 3. Найдите все простые несократимые дроби, лежащие между 0 и 1, знаменатель которых не превышает 11.
+from math import gcd
+def Task3():
+    print("\nЗадача 3:")
+    max_denominator = 11
+    fractions = simple_fractions(max_denominator)
+    print("Несократимые дроби между 0 и 1 с знаменателем не больше", max_denominator)
+    for fraction in fractions:
+        print(f"{fraction[0]}/{fraction[1]}")
 
+def is_coprime(a, b):
+    return gcd(a, b) == 1
+
+def simple_fractions(max_denominator):
+    fractions = []
+    for denominator in range(1, max_denominator + 1):
+        for numerator in range(1, denominator):
+            if is_coprime(numerator, denominator):
+                fractions.append((numerator, denominator))
+    return fractions
+Task3()
